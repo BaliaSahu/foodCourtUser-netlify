8b9c48e22e7e8aafc012cddb187f4e9d335f21b1
Balia Sahu <baliasahu7382@gmail.com>
2025-12-31 21:52:01 +0530

    Update myOrdersService.js

--- src/service/myOrdersService.js
+++ src/service/myOrdersService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "https://foodcourt-backend-production.up.railway.app"
+const API_URL = "https://foodcourt-backend-beob.onrender.com"
 
 export const placeOrder = async (data, token) => {
     const res = await axios.post(API_URL + "/create/order", data, {
